7c6d4c549c878d2988bfe36ccf2e93c41c3d4c92
Youssef Eldesouky <ywael.eldesouky@gmail.com>
2025-10-13 01:37:37 +0300

    Added Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import 'package:qadaa_prayer_tracker/Views/sign_in_screen.dart';
 import 'package:qadaa_prayer_tracker/Views/qadaa_missed.dart';
 import 'package:qadaa_prayer_tracker/Views/sign_up_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
